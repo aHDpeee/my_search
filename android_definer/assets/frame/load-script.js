dcf44bb9d14eb0d16fc79528d16d14233298f154
Andrey <andreyborisov08@yandex.ru>
2025-08-06 21:03:10 +0300

    888

--- android_definer/assets/frame/load-script.js
+++
@@ -1,7 +0,0 @@
-fetch('https://cse.google.com/cse.js?cx=...')
-  .then(res => res.text())
-  .then(code => {
-    const script = document.createElement('script');
-    script.textContent = code;
-    document.head.appendChild(script);
-  });
